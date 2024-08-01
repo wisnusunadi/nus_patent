c29dff24ce28489483c4aaa4a54e86d0dfa3aa69
wisnusunadi <barto7773@gmail.com>
2024-08-02 00:40:09 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -9,8 +15,65 @@ app.listen(PORT,() => {
     console.log("Express run " + PORT);
 })
 
-app.get("/api",(req,res)=>{
-    res.send('Hello Worlds')
+
+
+app.get("/api/kelas", async (req,res)=>{
+    const page = parseInt(req.query.page) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search_query || "";
+    const offset = limit * page;
+
+    const totalRows = await prisma.dataKelas.count({
+        where : {
+          OR : [
+            {
+                nama_eng : {
+                    contains : search
+                },
+            },
+                {
+                    nama_ind : {
+                        contains : search
+                    }
+                }
+          ]
+        }
+    });
+
+    const totalPage = Math.ceil(totalRows/limit);
+
+    const result = await prisma.dataKelas.findMany({
+        where : {
+            OR : [
+              {
+                  nama_eng : {
+                      contains : search
+                  },
+              },
+                  {
+                      nama_ind : {
+                          contains : search
+                      }
+                  }
+            ]
+          },
+          skip : offset,
+          take : limit ,
+          orderBy : {
+            id : 'asc'
+          }
+    });
+
+    const rePage = page+1;
+    res.json({
+        result: result,
+        page: page,
+        limit: limit,
+        totalRows: totalRows,
+        totalPage: totalPage,
+        number: rePage <= 1 ? 1 : rePage * 10
+    });
+
 })
 
 
