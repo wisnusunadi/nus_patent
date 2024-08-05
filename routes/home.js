ec97cfd399ef45dde0b12f46197a17fe8c485629
wisnusunadi <barto7773@gmail.com>
2024-08-05 21:23:14 +0700

    +

--- routes/home.js
+++ routes/home.js
@@ -9,63 +9,63 @@ app.use(cors());
 
 const prisma = new PrismaClient();
 
-router.get("/kelas", async (req,res)=>{
-    const page = parseInt(req.query.page) || 0;
-    const limit = parseInt(req.query.limit) || 10;
-    const search = req.query.search_query || "";
-    const offset = limit * page;
+// router.get("/kelas", async (req,res)=>{
+//     const page = parseInt(req.query.page) || 0;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const search = req.query.search_query || "";
+//     const offset = limit * page;
 
-    const totalRows = await prisma.dataKelas.count({
-        where : {
-          OR : [
-            {
-                nama_eng : {
-                    contains : search
-                },
-            },
-                {
-                    nama_ind : {
-                        contains : search
-                    }
-                }
-          ]
-        }
-    });
+//     const totalRows = await prisma.dataKelas.count({
+//         where : {
+//           OR : [
+//             {
+//                 nama_eng : {
+//                     contains : search
+//                 },
+//             },
+//                 {
+//                     nama_ind : {
+//                         contains : search
+//                     }
+//                 }
+//           ]
+//         }
+//     });
 
-    const totalPage = Math.ceil(totalRows/limit);
+//     const totalPage = Math.ceil(totalRows/limit);
 
-    const result = await prisma.dataKelas.findMany({
-        where : {
-            OR : [
-              {
-                  nama_eng : {
-                      contains : search
-                  },
-              },
-                  {
-                      nama_ind : {
-                          contains : search
-                      }
-                  }
-            ]
-          },
-          skip : offset,
-          take : limit ,
-          orderBy : {
-            id : 'asc'
-          }
-    });
+//     const result = await prisma.dataKelas.findMany({
+//         where : {
+//             OR : [
+//               {
+//                   nama_eng : {
+//                       contains : search
+//                   },
+//               },
+//                   {
+//                       nama_ind : {
+//                           contains : search
+//                       }
+//                   }
+//             ]
+//           },
+//           skip : offset,
+//           take : limit ,
+//           orderBy : {
+//             id : 'asc'
+//           }
+//     });
 
-    const rePage = page+1;
-    res.json({
-        result: result,
-        page: page,
-        limit: limit,
-        totalRows: totalRows,
-        totalPage: totalPage,
-        number: rePage <= 1 ? 1 : rePage * 10
-    });
+//     const rePage = page+1;
+//     res.json({
+//         result: result,
+//         page: page,
+//         limit: limit,
+//         totalRows: totalRows,
+//         totalPage: totalPage,
+//         number: rePage <= 1 ? 1 : rePage * 10
+//     });
 
-})
+// })
 
 module.exports = router;
