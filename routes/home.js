afba87656c5a98341d91249b6ccbc196bff9db66
wisnusunadi <barto7773@gmail.com>
2024-08-08 23:09:22 +0700

    filter + limit

--- routes/home.js
+++ routes/home.js
@@ -19,28 +19,10 @@ router.get("/kelas", async (req,res)=>{
     const limit = parseInt(req.query.limit) || 10;
     const search = req.query.search_query || "";
     const offset = limit * page;
+    const kategori = req.query.kategori || "all"
 
-    const totalRows = await prisma.dataKelas.count({
-        where : {
-          OR : [
-            {
-                nama_eng : {
-                    contains : search
-                },
-            },
-                {
-                    nama_ind : {
-                        contains : search
-                    }
-                }
-          ]
-        }
-    });
 
-    const totalPage = Math.ceil(totalRows/limit);
-
-    const result = await prisma.dataKelas.findMany({
-        where : {
+    let whereStatement = {
             OR : [
               {
                   nama_eng : {
@@ -53,7 +35,53 @@ router.get("/kelas", async (req,res)=>{
                       }
                   }
             ]
-          },
+    }
+
+
+
+    if (kategori == "barang") {
+        whereStatement = {
+          ...whereStatement,
+          AND: [
+            ...whereStatement.AND || [], // Preserve any existing AND conditions
+            {
+              kelas: {
+                in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34
+                ],
+              },
+            },
+          ],
+        };
+      }else if(kategori == 'jasa'){
+        whereStatement = {
+            ...whereStatement,
+            AND: [
+              ...whereStatement.AND || [], // Preserve any existing AND conditions
+              {
+                kelas: {
+                  in: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45],
+                },
+              },
+            ],
+          };
+      }
+
+      
+
+
+
+
+
+    
+
+    const totalRows = await prisma.dataKelas.count({
+        where : whereStatement
+    });
+
+    const totalPage = Math.ceil(totalRows/limit);
+
+    const result = await prisma.dataKelas.findMany({
+        where : whereStatement,
           skip : offset,
           take : limit ,
           orderBy : {
